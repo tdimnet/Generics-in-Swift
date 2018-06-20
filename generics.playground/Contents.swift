8dbbb0a756296caeda2c023c149da8af9983d182
tdimnet <thomas.dimnet@gmail.com>
2018-06-20 17:37:51 +0200

    Write the test case for using generics

--- generics.playground/Contents.swift
+++ generics.playground/Contents.swift
@@ -1,5 +1,38 @@
-//: Playground - noun: a place where people can play
+// Generics
 
-import UIKit
+func swapInts(_ a: inout Int, _ b: inout Int) {
+    let tempA = a
+    a = b
+    b = tempA
+}
 
-var str = "Hello, playground"
+var d = 10
+var e = 12
+
+swapInts(&d, &e)
+
+d
+e
+
+var g = "Thomas"
+var h = "Dimnet"
+
+// swapInts(&g, &h) -> will not work because we attempt to work with integers
+
+func swapStrings(_ a: inout String, _ b: inout String) {
+    let tempA = a
+    a = b
+    b = tempA
+}
+
+swapStrings(&g, &h)
+
+g
+h
+
+// Below, this could be a good idea but we lose the type safety of Swift.
+func swapAny(_ a: inout Any, _ b: inout Any) {
+    let tempA = a
+    a = b
+    b = tempA
+}
