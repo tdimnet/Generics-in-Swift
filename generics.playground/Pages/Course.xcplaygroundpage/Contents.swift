cb189cd5a30c0e005e124bc71163c27033e35aa1
tdimnet <thomas.dimnet@gmail.com>
2018-06-26 12:01:29 +0200

    Add generics with multiple parameters

--- generics.playground/Pages/Course.xcplaygroundpage/Contents.swift
+++ generics.playground/Pages/Course.xcplaygroundpage/Contents.swift
@@ -17,3 +17,22 @@ swapValues(&d, &e)
 swapValues(&g, &h)
 
 // swapValues(&d, &g) -> this will fail
+
+
+// Generic with multiple type parameters
+func transform<T, U>(arg: T, operation: (T) -> U) -> U { // Generic function is more than defining rule than doing a specific operation
+    return operation(arg)
+}
+
+func stringToInt(_ a: String) -> Int {
+    guard let value = Int(a) else { fatalError() }
+    return value
+}
+
+transform(arg: "1", operation: stringToInt)
+
+func intToString(_ a: Int) -> String {
+    return String(a)
+}
+
+transform(arg: 12, operation: intToString)
