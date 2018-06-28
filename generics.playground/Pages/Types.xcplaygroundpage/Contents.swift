0746ec3ccfbb348a01f37000c31b967a1b392469
tdimnet <thomas.dimnet@gmail.com>
2018-06-28 15:25:49 +0200

    Start working with LinkLists

--- generics.playground/Pages/Types.xcplaygroundpage/Contents.swift
+++ generics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -9,6 +9,69 @@ let streetName = Optional.some("123 Street")
 var errorCodes = Dictionary<Int, String>()
 
 
+// Linked Lists
+
+class LinkedListNode<Key> {
+    let key: Key
+    
+    var next: LinkedListNode?
+    weak var previous: LinkedListNode?
+    
+    init(key: Key) {
+        self.key = key
+    }
+}
+
+class LinkedList<Element> {
+    typealias Node = LinkedListNode<Element>
+    
+    private var head: Node?
+    
+    var first: Node? {
+        return head
+    }
+    
+    var last: Node? {
+        if var node = head {
+            while node.next != nil {
+                node = node.next
+            }
+            return node
+        } else {
+            return nil
+        }
+    }
+}
+
+let list = LinkedList<String>()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
